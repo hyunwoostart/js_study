0d81f6878d5b71914323d5dcd67775f36e1b9b09
hwji <knjhw@daum.net>
2023-12-09 22:13:06 +0900

    bind() 첫번째

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ function main() {
     console.log(this);
 }
 
-const object = {
-    name: "별코딩",
-    smallObject: {
-        name: "작은 별코딩",
-        main, 
-    },
-};
-
-object.smallObject.main();
-
-
+const mainBind = main.bind({ name: "hi" });
+const mainBindBind = mainBind.bind({})
+mainBindBind();
