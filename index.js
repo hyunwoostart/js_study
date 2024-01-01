9e1f47afb62290419a535cc344e85d362491e5fe
hwji <knjhw@daum.net>
2024-01-01 23:53:34 +0900

    매개변수와 인수 사용

--- index.js
+++ index.js
@@ -1,48 +1,29 @@
-// 함수 return 이해하기
-function a () {}
-
-a();
-
-function a() {
-    return 10;
+// // 매개변수와 인수 사용하기
+function a(parameter) {
+    console.log(parameter);
 }
+a('argument');
 
-a();
-
-function a() {}
-// 두 코드는 동일함
-function a() {
-    return undefined;
+function a(w, x, y, z) {
+    console.log(w, x, y, z);
+    console.log(arguments);
 }
+a('Hello', 'Parameter', 'Argument');
 
-function a() {
-    console.log('a');
+function a(w, x) {
+    console.log(w, x);
 }
-// 두 코드는 동일함
-function a() {
-    console.log('a');
-    return undefined;
-}
+a('Hello', 'Parameter', 'Argument');
 
-function a() {
-    return 10;
+function add(x, y) {
+    return x + y;
 }
-console.log(a());
+console.log(add(3, 5));
+console.log(add(8, 7));
 
-function a() {
-    return 10;
+// 퀴즈 - 매개변수로 x, y, z을 받아 곱한 값을 반환하는 multiply 함수를 만들어보세요. 단, 화살표 함수로 만드세요.
+const multiply = (x, y, z) => {
+    return x * y * z;
 }
+console.log(multiply(3,5,5));
 
-const b = a();
-console.log(b);
-
-function a() {
-    console.log('Hello');
-    return;
-}
-a();
-
-function a() {
-    console.log('실행됩니다.');
-}
-a();
