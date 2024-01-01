d58d00b029143833a5e20e3be3a82332768951a1
hwji <knjhw@daum.net>
2024-01-01 23:37:43 +0900

    함수 return

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-// 배열에서 반복하기
-// const target = ['가', '나', '다', '라', '마'];
-// let i = 0;
-// while (i < target.length) {
-//     console.log(target[i]);
-//     i++;
-// }
+// 함수 return 이해하기
+function a () {}
 
-// const target2 = ['가', '나', '다', '라', '마'];
-//     for (let i = 0; i < target2.length; i ++) {
-//         console.log(target[i]);
-//     }
+a();
 
-// 퀴즈 - 다음 배열에서 '라'를 모두 제거하세요. indexOf와 splice를 사용하세요. const arr = ['가', '라', '다', '라', '마', '라'];
-const arr = ['가', '라', '다', '라', '마', '라'];
-let index = arr.indexOf('라');
-while (index > -1) {
-    arr.splice(index, 1);
-    index = arr.indexOf('라');
+function a() {
+    return 10;
 }
 
-console.log(arr);
+a();
+
+function a() {}
+// 두 코드는 동일함
+function a() {
+    return undefined;
+}
+
+function a() {
+    console.log('a');
+}
+// 두 코드는 동일함
+function a() {
+    console.log('a');
+    return undefined;
+}
+
+function a() {
+    return 10;
+}
+console.log(a());
+
+function a() {
+    return 10;
+}
+
+const b = a();
+console.log(b);
+
+function a() {
+    console.log('Hello');
+    return;
+}
+a();
+
+function a() {
+    console.log('실행됩니다.');
+}
+a();
