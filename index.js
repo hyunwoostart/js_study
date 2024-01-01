79e6d0feb28068c1672bfc783eebae8e65d05d61
hwji <knjhw@daum.net>
2024-01-01 22:13:42 +0900

    배열에서 요소 찾기

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// 배열에서 요소 제거하기
-const target = ['가', '나', '다', '라', '마'];
-target.pop();
-console.log(target);
+// 배열에서 요소 찾기
+// const target = ['가', '나', '다', '라', '마'];
+// const result = target.includes('다');
+// const result2 = target.includes('카');
+// console.log(result);
+// console.log(result2);
 
-const target = ['가', '나', '다', '라', '마'];
-target.shift();
-console.log(target);
-
-const target = ['가', '나', '다', '라', '마'];
-target.splice(2, 2);
-console.log(target);
-
-const target = ['가', '나', '다', '라', '마'];
-target.splice(1);
-console.log(target);
-
-const target = ['가', '나', '다', '라', '마'];
-target.splice(1, 3, '타', '파');
-console.log(target);
+const target = ['라', '나', '다', '라', '다'];
+const result = target.indexOf('다');
+const result2 = target.lastIndexOf('라');
+const result3 = target.indexOf('가');
+console.log(result);
+console.log(result2);
+console.log(result3);
