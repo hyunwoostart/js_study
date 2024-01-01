e7b1b23db14d40c454bccd2b44d9c4e20c9d60ee
hwji <knjhw@daum.net>
2024-01-01 22:30:29 +0900

    배열 반복하기

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-// 배열에서 요소 찾기
+// 배열에서 반복하기
 // const target = ['가', '나', '다', '라', '마'];
-// const result = target.includes('다');
-// const result2 = target.includes('카');
-// console.log(result);
-// console.log(result2);
+// let i = 0;
+// while (i < target.length) {
+//     console.log(target[i]);
+//     i++;
+// }
 
-const target = ['라', '나', '다', '라', '다'];
-const result = target.indexOf('다');
-const result2 = target.lastIndexOf('라');
-const result3 = target.indexOf('가');
-console.log(result);
-console.log(result2);
-console.log(result3);
+// const target2 = ['가', '나', '다', '라', '마'];
+//     for (let i = 0; i < target2.length; i ++) {
+//         console.log(target[i]);
+//     }
+
+// 퀴즈 - 다음 배열에서 '라'를 모두 제거하세요. indexOf와 splice를 사용하세요. const arr = ['가', '라', '다', '라', '마', '라'];
+const arr = ['가', '라', '다', '라', '마', '라'];
+let index = arr.indexOf('라');
+while (index > -1) {
+    arr.splice(index, 1);
+    index = arr.indexOf('라');
+}
+
+console.log(arr);
