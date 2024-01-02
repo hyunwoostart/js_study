0bc630903c4c9597ec2e9fe219f65ae463cc31c5
hwji <knjhw@daum.net>
2024-01-02 23:23:15 +0900

    객체 리터럴

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-// // 매개변수와 인수 사용하기
-function a(parameter) {
-    console.log(parameter);
-}
-a('argument');
+// 객체 리터럴
+const zerocho = {
+    name: '조현영',
+    year: 1994,
+    month: 8,
+    date: 12,
+    gender: 'M',
+};
 
-function a(w, x, y, z) {
-    console.log(w, x, y, z);
-    console.log(arguments);
-}
-a('Hello', 'Parameter', 'Argument');
+// const 객체 = {
+//     속성이름: 속성값,
+// }
 
-function a(w, x) {
-    console.log(w, x);
-}
-a('Hello', 'Parameter', 'Argument');
+console.log(zerocho.name);
+console.log(zerocho['name']);
+console.log(zerocho.date);
+console.log(zerocho['date']);
 
-function add(x, y) {
-    return x + y;
-}
-console.log(add(3, 5));
-console.log(add(8, 7));
-
-// 퀴즈 - 매개변수로 x, y, z을 받아 곱한 값을 반환하는 multiply 함수를 만들어보세요. 단, 화살표 함수로 만드세요.
-const multiply = (x, y, z) => {
-    return x * y * z;
-}
-console.log(multiply(3,5,5));
-
+const name = 'date';
+console.log(zerocho['name']);
+console.log(zerocho[name]);
+console.log(zerocho.hello);
