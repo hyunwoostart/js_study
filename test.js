95a1ef972dd279b3343c8a3c7ba3785c3ab6c641
hwji <knjhw@daum.net>
2024-01-10 23:47:27 +0900

    숫자야구 게임 v1

---
+++ test.js
@@ -0,0 +1,31 @@
+/*
+2에서 5까지의 숫자를 뽑고싶습니다.
+코드 4가지 경우
+*/
+
+// 코드1
+const answer1 = [];
+for (let n = 0; n < (가); n++) {
+    answer1.push(n+2);
+}
+
+// 코드2
+const answer2 = [];
+for (let n = 0; n <= (나); n++) {
+    answer2.push(n+2);
+}
+
+// 코드3
+const answer3 = [];
+for (let n = 1; n < (다); n++) {
+    answer3.push(n+1);
+}
+
+// 코드4
+const answer4 = [];
+for (let n = 1; n <= (라); n++) {
+    answer4.push(n+1);
+}
+
+// 각각 (가),(나),(다),(라) 에 들어갈 숫자
+// 4, 3, 5, 4
