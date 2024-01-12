7d4be080f518bf78ea47c4f789f384be986f4fa4
hwji <knjhw@daum.net>
2024-01-12 15:18:52 +0900

    캘린더 연습

---
+++ calendar/replay.js
@@ -0,0 +1,21 @@
+let date = new Date();
+
+const rederCalender = () => {
+    const viewYear = date.getFullYear();
+    const viewMonth = date.getMonth();
+
+    document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`
+
+    const prevLast = new Date(viewYear, viewMonth, 0);
+    const thisLast = new Date(viewYear, viewMonth + 1, 0);
+
+    const PLDate = prevLast.getDate();
+    const PLDay = prevLast.getDay();
+
+    const TLDate = thisLast.getDate();
+    const TLDay = thisLast.getDay();
+
+    const prevDates = [];
+    const thisDates = [... Array(TLDate + 1).keys()].slice
+
+}   
