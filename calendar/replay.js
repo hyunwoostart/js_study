9e3031aa279fe73b47dfa90b67f17834ac72e369
hwji <knjhw@daum.net>
2024-01-12 22:06:41 +0900

    숫자야구 완성

--- calendar/replay.js
+++ calendar/replay.js
@@ -1,6 +1,6 @@
 let date = new Date();
 
-const rederCalender = () => {
+const renderCalender = () => {
     const viewYear = date.getFullYear();
     const viewMonth = date.getMonth();
 
@@ -16,6 +16,53 @@ const rederCalender = () => {
     const TLDay = thisLast.getDay();
 
     const prevDates = [];
-    const thisDates = [... Array(TLDate + 1).keys()].slice
+    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+    const nextDates = [];
 
-}   
+    if (PLDay !== 6) {
+        for (let i = 0; i < PLDay + 1; i++) {
+            prevDates.unshift(PLDate - i);
+        }
+    }
+
+    for (let i = 1; i < 7 - TLDay; i++) {
+        nextDates.push(i);
+    }
+
+    const dates = prevDates.concat(thisDates, nextDates);
+    const firstDateIndex = dates.indexOf(1);
+    const lastDateIndex = dates.lastIndexOf(TLDate);
+
+    dates.forEach((date, i) => {
+        const condition = i >= firstDateIndex && i < lastDateIndex + 1 ? 'this' : 'other';
+            dates[i] = `<div class="date"><span class=${condition}>${date}</span></div>`;
+        });
+
+    document.querySelector('.dates').innerHTML = dates.join('');
+    const today = new Date();
+    if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+        for (let date of document.querySelectorAll('.this')) {
+            if (+date.innerText === today.getDate()) {
+                date.classList.add('today');
+                break;
+            }
+        }
+    }
+};
+
+renderCalender();
+
+const prevMonth = () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalender();
+}
+
+const nextMonth = () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalender();
+}
+
+const goToday = () => {
+    date = new Date();
+    renderCalender();
+}
